78ddd526c4b8f960da81a491d8ce492acc4c5fd5
Robert Sörensen <robert.sorensen44@gmail.com>
2021-04-13 15:09:04 +0200

    organizer edit events page

---
+++ ProjektASP/Pages/OrganizerOrganizeEvent.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ProjektASP.Data;
+using ProjektASP.Models;
+
+namespace ProjektASP.Pages
+{
+    [Authorize(Roles = "Organizer")]
+    public class OrganizerOrganizeEventModel : PageModel
+    {
+        private readonly EventsDbContext _context;
+        private readonly UserManager<Attendee> _userManager;
+
+        public OrganizerOrganizeEventModel(
+            EventsDbContext context,
+            UserManager<Attendee> userManager
+            )
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public IList<Event> Events { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var userId = _userManager.GetUserId(User);
+            var user = await _context.Attendees
+                .Where(u => u.Id == userId)
+                .Include(u => u.Events)
+                .FirstOrDefaultAsync();
+
+            Events = user.Events;
+
+        }
+    }
+}
