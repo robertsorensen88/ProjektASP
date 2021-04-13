c1426725818189029afa83db1568473953298cef
Robert Sörensen <robert.sorensen44@gmail.com>
2021-04-13 14:39:55 +0200

    när organizer skapar event läggs det till i myEvent

--- ProjektASP/Pages/OrganizerAddEvent.cshtml.cs
+++ ProjektASP/Pages/OrganizerAddEvent.cshtml.cs
@@ -2,21 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using ProjektASP.Data;
 using ProjektASP.Models;
 
 namespace ProjektASP.Pages
 {
+    [Authorize(Roles = "Organizer")]
     public class OrganizerAddEventModel : PageModel
     {
         private readonly EventsDbContext _context;
+        private readonly UserManager<Attendee> _userManager;
 
-        public OrganizerAddEventModel(EventsDbContext context)
+        public OrganizerAddEventModel(EventsDbContext context, UserManager<Attendee> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult OnGet()
@@ -26,16 +32,27 @@ namespace ProjektASP.Pages
 
         [BindProperty]
         public Event Event { get; set; }
+        public Attendee Attendee { get; set; }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int? id)
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
+            var GetId = _userManager.GetUserId(User);
 
-            _context.Events.Add(Event);
+           
+
+            var user = await _context.Attendees
+                .Where(u => u.Id == GetId)
+                .Include(u => u.Events)
+                .FirstOrDefaultAsync();
+
+            user.Events.Add(Event);
+          
+
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
